fee370b1da43ef86a48e7084992b9fa592ee98d2
Onurkan Bakırcı <onurkan_bkrc@hotmail.com>
2025-04-13 13:03:41 +0300

    feat: enhance layout metadata with Twitter integration and cart functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,43 @@
+
 import { GeistSans } from 'geist/font/sans';
+import { getCart } from 'lib/static';
+import { ensureStartsWith } from 'lib/utils';
+import { cookies } from 'next/headers';
 import { ReactNode } from 'react';
 import './globals.css';
 
-const { SITE_NAME } = process.env;
+const { TWITTER_CREATOR, TWITTER_SITE, SITE_NAME } = process.env;
+const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : 'http://localhost:3000';
+const twitterCreator = TWITTER_CREATOR ? ensureStartsWith(TWITTER_CREATOR, '@') : undefined;
+const twitterSite = TWITTER_SITE ? ensureStartsWith(TWITTER_SITE, 'https://') : undefined;
 
 export const metadata = {
+  metadataBase: new URL(baseUrl),
   title: {
     default: SITE_NAME!,
     template: `%s | ${SITE_NAME}`
-  }
+  },
+  robots: {
+    follow: true,
+    index: true
+  },
+  ...(twitterCreator &&
+    twitterSite && {
+    twitter: {
+      card: 'summary_large_image',
+      creator: twitterCreator,
+      site: twitterSite
+    }
+  })
 };
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
+  const cartId = (await cookies()).get('cartId')?.value;
+  // Don't await the fetch, pass the Promise to the context provider
+  const cart = getCart(cartId);
+
   return (
     <html lang="en" className={GeistSans.variable}>
       <body className="bg-neutral-50">
